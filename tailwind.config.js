2f2c81df30d470f2fd374b702c813a5517c20080
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2022-12-22 01:57:08 +0000

    Configure PostCSS, TailwindCSS and Gulp

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        cyan: 'hsl(178, 100%, 50%)',
+        white: 'hsl(0, 0%, 100%)',
+        'blue-300': 'hsl(215, 51%, 70%)',
+        'blue-500': 'hsl(215, 32%, 27%)',
+        'blue-600': 'hsl(216, 50%, 16%)',
+        'blue-700': 'hsl(217, 54%, 11%)',
+      },
+      fontFamily: {
+        sans: ['Outfit', 'sans-serif'],
+      },
+      fontSize: {
+        sm: '0.6875rem',
+        base: '1.025rem',
+        xl: '1.4rem',
+      },
+      letterSpacing: {
+        tighter: '-.025rem',
+        tight: '-.0125rem'
+      },
+      boxShadow: {
+        DEFAULT: '0 5.9px 7.9px rgb(0 0 0 / 12%), 0 47px 63px rgb(0 0 0 / 24%)',
+        md: '0 5.9px 7.9px rgb(0 0 0 / 12%), 0 47px 63px rgb(0 0 0 / 24%)'
+      }
+    },
+  },
+  plugins: [],
+}
